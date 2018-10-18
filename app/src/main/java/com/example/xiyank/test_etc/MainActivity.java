7dd264576e2f8b89c31eeec831d0bbdac51751f7
= <=>
2018-10-18 09:53:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xiyank/test_etc/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.xiyank.test_etc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
